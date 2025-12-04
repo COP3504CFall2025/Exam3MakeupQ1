dec76916fc18f53d9052fc2ac8bdbc44b65e5ce1
MichaelLLink <32107125+MichaelLLink@users.noreply.github.com>
2025-12-04 13:12:24 -0500

    Create Celebrity.cpp

---
+++ Celebrity.cpp
@@ -0,0 +1,9 @@
+#include "celebrity.hpp"
+#include <stack>        // or use ArrayStack
+// #include "ArrayStack.hpp"
+
+int findCelebrity(const std::vector<std::vector<int>>& mat)
+{
+    // Write your code here :)
+    return -1; // placeholder
+}
