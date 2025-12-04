83aec34bb83139daa4877d5408003e5f0f13c4eb
MichaelLLink <32107125+MichaelLLink@users.noreply.github.com>
2025-12-04 14:16:01 -0500

    Delete Celebrity.cpp

--- Celebrity.cpp
+++
@@ -1,9 +0,0 @@
-#include "celebrity.hpp"
-#include <stack>        // or use ArrayStack
-// #include "ArrayStack.hpp"
-
-int findCelebrity(const std::vector<std::vector<int>>& mat)
-{
-    // Write your code here :)
-    return -1; // placeholder
-}
